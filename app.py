1c6310acce31c5fa32a591020b457b06e9ed715c
Rakeshcool <rakeshcool315@gmail.com>
2023-07-29 00:53:10 +0530

    added new logger but not working properly

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ import psutil
 from flask import Flask, render_template, jsonify
 from datetime import datetime
 
+# from csv_logger import CsvLogger
+# import logging
+
+
 app = Flask(__name__)
 
 
@@ -27,6 +31,31 @@ def get_metrics():
         disk_metric=disk_metric,
         message=message,
     )
+
+
+# filename = "logs/log.csv"
+# delimiter = ","
+# level = logging.INFO
+# custom_additional_levels = ["logs_a", "logs_b", "logs_c"]
+# fmt = f"%(asctime)s{delimiter}%(levelname)s{delimiter}%(message)s"
+# datefmt = "%Y/%m/%d %H:%M:%S"
+# max_size = 1024  # 1 kilobyte
+# max_files = 4  # 4 rotating files
+# header = ["date", "level", "value_1", "value_2"]
+
+# # Creat logger with csv rotating handler
+# csvlogger = CsvLogger(
+#     filename=filename,
+#     delimiter=delimiter,
+#     level=level,
+#     add_level_names=custom_additional_levels,
+#     add_level_nums=None,
+#     fmt=fmt,
+#     datefmt=datefmt,
+#     max_size=max_size,
+#     max_files=max_files,
+#     header=header,
+# )
 
 
 if __name__ == "__main__":
