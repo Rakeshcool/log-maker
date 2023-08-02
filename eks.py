f3a4bd01d2875b9348f781e2661150e60e0a43ca
Rakeshcool <rakeshcool315@gmail.com>
2023-08-02 12:59:48 +0530

    edited the requirement.txt file

--- eks.py
+++ eks.py
@@ -1,4 +1,4 @@
-#create deployment and service
+# create deployment and service
 from kubernetes import client, config
 
 # Load Kubernetes configuration
@@ -12,45 +12,34 @@ deployment = client.V1Deployment(
     metadata=client.V1ObjectMeta(name="my-flask-app"),
     spec=client.V1DeploymentSpec(
         replicas=1,
-        selector=client.V1LabelSelector(
-            match_labels={"app": "my-flask-app"}
-        ),
+        selector=client.V1LabelSelector(match_labels={"app": "my-flask-app"}),
         template=client.V1PodTemplateSpec(
-            metadata=client.V1ObjectMeta(
-                labels={"app": "my-flask-app"}
-            ),
+            metadata=client.V1ObjectMeta(labels={"app": "my-flask-app"}),
             spec=client.V1PodSpec(
                 containers=[
                     client.V1Container(
                         name="my-flask-container",
                         image="568373317874.dkr.ecr.us-east-1.amazonaws.com/my_monitoring_app_image:latest",
-                        ports=[client.V1ContainerPort(container_port=5000)]
+                        ports=[client.V1ContainerPort(container_port=5000)],
                     )
                 ]
-            )
-        )
-    )
+            ),
+        ),
+    ),
 )
 
 # Create the deployment
 api_instance = client.AppsV1Api(api_client)
-api_instance.create_namespaced_deployment(
-    namespace="default",
-    body=deployment
-)
+api_instance.create_namespaced_deployment(namespace="default", body=deployment)
 
 # Define the service
 service = client.V1Service(
     metadata=client.V1ObjectMeta(name="my-flask-service"),
     spec=client.V1ServiceSpec(
-        selector={"app": "my-flask-app"},
-        ports=[client.V1ServicePort(port=5000)]
-    )
+        selector={"app": "my-flask-app"}, ports=[client.V1ServicePort(port=5000)]
+    ),
 )
 
 # Create the service
 api_instance = client.CoreV1Api(api_client)
-api_instance.create_namespaced_service(
-    namespace="default",
-    body=service
-)
+api_instance.create_namespaced_service(namespace="default", body=service)
